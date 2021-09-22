1bb68d75092bffeca2aab669c6aefddc079d7cd0
Alexey <leha.seden@gmail.com>
2021-09-22 09:49:20 +0300

    Third

--- prog2.cpp
+++ prog2.cpp
@@ -8,40 +8,44 @@ namespace p2 {
 	}
 	int dialog(double& r, double& t) {
 		int a;
+		int help;
 		const char* opt[] = { "1.Enter radius", "2.S","3.L","4.R","5.Coords","6.Char form","7.Exit"};
 		for (int i = 0; i < 7; i++) {
 			std::cout << opt[i] << std::endl;
 		}
 		std::cout << "Choose one option" << std::endl;
-		getNum(a);
-		if (a < 1 && a>7) {
+		help=getNum(a,"int");
+		if ((a < 1 || a>7)) {
 			std::cout << "Error, try again" << std::endl;
 		}
-		while (a < 1 && a>7) {
-			getNum(a);
-			if (a < 1 && a>7) {
+		while (a < 1 || a>7) {
+			help=getNum(a,"int");
+			if ((a < 1 && a>7)) {
 				std::cout << "Error, try again" << std::endl;
 			}
 		}
 		if (a == 1) {
 			std::cout << "enter radius" << std::endl;
-			getNum(r);
+			help = getNum(r,"double");
+			if (help == -1) { return 7; }
 			return a;
 		}
 		if (a == 2) {
-			return a;
+			 return a;
 		}
 		if (a == 3) {
 			return a;
 		}
 		if (a == 4) {
 			std::cout << "enter t" << std::endl;
-			getNum(t);
+			help = getNum(t,"double");
+			if (help == -1) { return 7; }
 			return a;
 		}
 		if (a == 5) {
 			std::cout << "enter t" << std::endl;
-			getNum(t);
+			help = getNum(t,"double");
+			if (help == -1) { return 7; }
 			return a;
 		}
 		if (a == 6) { return a; }
--- prog2.h
+++ prog2.h
@@ -6,45 +6,63 @@
 
 namespace p2 {
 	template <class T>
-	int getNum(T& a)
-	{
-		std::cin >> a;
-		if (!std::cin.good())
-			return -1;
-		return 1;
+	int getNum(T& a, const char* text) {
+		bool flag = false;
+		std::cout << text << std::endl;
+		do {
+			if (flag == true) {
+				std::cout << "Error, try again:";
+				flag = false;
+			}
+			std::cin >> a;
+			if (std::cin.eof() || std::cin.bad()) {
+				return -1;
+			}
+			else if (std::cin.fail()) {
+				std::cin.clear();
+				std::cin.ignore(10000, '\n');
+				flag = true;
+			}
+
+		} while (flag == true);
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+		return 0;
 	}
+
 	class Nefr {
 	private:
 		double r;
 	public:
 		Nefr() { r = 0; };
 		void Set_R(double a) { r = a; }
-		double S() {
+		double S() const {
 			return r*6*r*M_PI;  //task 5
 		}
-		double L() {
+		double L() const {
 			return 12 * r; // task 3
 		}
-		double Rad(double t) {
+		double Rad(double t) const {
 			return abs((3 * r / 2) * sin(t)); // task 4
 		}
-		double X(double t) {
+		double X(double t) const {
 			return (3 * r / 2) * cos(t) - (r / 2) * cos(3 * t);  // task 6
 		}
-		double Y(double t) {
+		double Y(double t) const {
 			return (3 * r / 2) * sin(t) - (r / 2) * sin(3 * t);
 		}
-		void form() { // task 7
+		void form() const { // task 7
 			const char* help = "(x^2 + y^2 - )^3 = *y^2";
 			double a = r / 2;
-			int len = strlen(help);
+			int len = strlen(help)+1;
 			char num[100];
 			sprintf(num, "%.2f", r*r);
 			len += strlen(num);
 			sprintf(num, "%.2f", 108*a*a*a*a);
 			len += strlen(num);
+			
 			char* str = new char[len];
-			sprintf(str,"%s%.2f%s%2.f%s","(x^2 + y^2 - ",r*r,")^3 = ",108*a*a*a*a,"*y^2");
+			sprintf(str,"%s%.2f%s%.2f%s","(x^2 + y^2 - ",r*r,")^3 = ",108*a*a*a*a,"*y^2");
 			std::cout << "\n" << str << std::endl;
 			delete[] str;
 		}
@@ -52,6 +70,5 @@ namespace p2 {
 	int coords(double t, double& x, double& y, Nefr a);
 	int dialog(double &r, double& t);
 	void ch(int num, double x, double y, double r, double t, Nefr& nefr);
-	
 }
 #endif
